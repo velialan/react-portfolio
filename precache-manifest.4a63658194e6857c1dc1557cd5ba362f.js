796a8723ce2bd3790cd196f7e9f42c9bf4b51a07
velialan <veli.alan03@gmail.com>
2020-08-27 16:14:44 +0300

    Updates

---
+++ precache-manifest.4a63658194e6857c1dc1557cd5ba362f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533ceda881b11bc523d5208d10853796",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "332be46a432a3904e899",
+    "url": "/react-portfolio/static/css/main.a80e7b0f.chunk.css"
+  },
+  {
+    "revision": "43b155b1382bd030f5aa",
+    "url": "/react-portfolio/static/js/2.eb97e1c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.eb97e1c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332be46a432a3904e899",
+    "url": "/react-portfolio/static/js/main.514baafa.chunk.js"
+  },
+  {
+    "revision": "a9912bc232d33598c923",
+    "url": "/react-portfolio/static/js/runtime-main.04cb37bb.js"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/react-portfolio/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/react-portfolio/static/media/map-image.f40ed5b1.png"
+  }
+]);
